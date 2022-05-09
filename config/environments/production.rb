1f1aed3920f841f90d3fad3455bc0a0e26b7cb9e
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-05-09 21:25:27 +0200

    production heroku try ena

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = {:host => 'https://online--books.herokuapp.com/'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :domain => "gmail.com",
+    :user_name => "infolibrary7@gmail.com", #your gmail id
+    :password => "onlineLibrary", #your gmail password
+    :port    => 25,
+    :authentication => "plain",
+    :enable_starttls_auto => true
+    :domain  => 'https://online--books.herokuapp.com/'
+  }
 end
