bb35c05d3263b46478339238f1f1b786bbbcf312
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-05-09 21:48:23 +0200

    heroku production 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,16 +119,29 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-  config.action_mailer.default_url_options = {:host => 'https://online--books.herokuapp.com/'}
+  # config.action_mailer.default_url_options = {:host => 'https://online--books.herokuapp.com/'}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :domain => "gmail.com",
+  #   :user_name => "infolibrary7@gmail.com", #your gmail id
+  #   :password => "onlineLibrary", #your gmail password
+  #   :port    => 25,
+  #   :authentication => "plain",
+  #   :enable_starttls_auto => true,
+  #   :domain  => 'https://online--books.herokuapp.com/'
+  # }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'https://online--books.herokuapp.com/', protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-    :domain => "gmail.com",
+    :port => 587,
     :user_name => "infolibrary7@gmail.com", #your gmail id
     :password => "onlineLibrary", #your gmail password
-    :port    => 25,
-    :authentication => "plain",
-    :enable_starttls_auto => true,
-    :domain  => 'https://online--books.herokuapp.com/'
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
+
 end
