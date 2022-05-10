3d05451719f4f9c06286fb090028f60a3c8d10af
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-05-10 12:40:34 +0200

    nepotreben commit

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,12 @@ ActiveRecord::Schema.define(version: 2022_05_10_093747) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "admin"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "name"
+    t.integer "admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
