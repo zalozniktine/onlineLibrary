746cfeff470edbab39c199a0b5aac77a7e9c7f25
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-05-16 20:26:20 +0200

    Mailer dela pravilno-zravn confim dobis se set pas

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,9 @@ class UserMailer < ApplicationMailer
     def pass_email
         @user = params[:user]
         #@url  = 'http://localhost:3000/users/sign_in'
-        @url  = 'https://online--books.herokuapp.com/users/password/new'
-        mail(to: @user.email, subject: 'onlineLibrary')
+        #@url  = 'https://online--books.herokuapp.com/users/password/new'
+        #@testnitoken = params[:user].signed_id(purpose: "password_reset")
+        @token = @user.send_reset_password_instructions
+        #mail(to: @user.email, subject: 'onlineLibrary')
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   get 'about_book',to:"home#about_book"
   get 'preview',to:"home#book_preview"
   get 'admin',to:"admin#admin"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  get "password/reset/edit", to: "password_resets#update"
+
   
 
   devise_for :users, controllers: {
